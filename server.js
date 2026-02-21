22586fb4e91c5ded08695fd42a281f59f67cdc75
DevRowdra <rowdradev23@gmail.com>
2026-02-21 14:17:51 +0600

    make user schema using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/db");
-// console.log(process.env.MONGO_URI);
 connectToDB();
 app.listen(3000, () => {
   console.log(`Server is runing on port ${process.env.PORT}`);
